45cf475e74106f7ad5c92dd257d59146a8e5553f
longnar <151906652+longnar@users.noreply.github.com>
2026-08-24 10:19:35 +0000

    fisrt commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express= require("express")
 const PORT = process.env.PORT || 9000;
 const app = express()
 
+app.get('/api/get', (req, res) => {
+    res.json({
+        message: 'This is a GET request!'
+    });
+});
+
 app.listen(PORT, ()=>{
-    console.log('Server is running}');
+    console.log(`Server is running on port ${PORT}`);
 });
